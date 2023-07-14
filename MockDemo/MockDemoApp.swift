30ee001a42b650267ed8cff1b37bf809cf119771
Prabjot Singh <prabjot.singh@andersencorp.com>
2023-07-14 12:16:58 -0400

    Project setup

---
+++ MockDemo/MockDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MockDemoApp.swift
+//  MockDemo
+//
+//  Created by M_2195552 on 2023-07-14.
+//
+
+import SwiftUI
+
+@main
+struct MockDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
