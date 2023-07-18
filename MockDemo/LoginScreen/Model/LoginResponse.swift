993b388bbe46473cf375953a7ab76ac4d87f4507
Prabjot Singh <prabjot.singh@andersencorp.com>
2023-07-18 11:11:22 -0400

    Implemented Login screen with API integration

---
+++ MockDemo/LoginScreen/Model/LoginResponse.swift
@@ -0,0 +1,12 @@
+//
+//  LoginResponse.swift
+//  MockDemo
+//
+//  Created by M_2195552 on 2023-07-17.
+//
+
+import Foundation
+
+struct LoginResponse: Codable {
+    let success: Bool
+}
---
+++ MockDemo/LoginScreen/View/DashboardView.swift
@@ -0,0 +1,23 @@
+//
+//  DashboardView.swift
+//  MockDemo
+//
+//  Created by M_2195552 on 2023-07-17.
+//
+
+import SwiftUI
+
+struct DashboardView: View {
+    var body: some View {
+        VStack {
+            Text("Hello World")
+            .navigationTitle("Dashboard")
+        }
+    }
+}
+
+struct DashboardView_Previews: PreviewProvider {
+    static var previews: some View {
+        DashboardView()
+    }
+}
